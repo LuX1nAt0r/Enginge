73eb48632173987e8bbb448d91da1e816a4666a9
LuX1nAt0r <l.landsteiner@t-online.de>
2021-06-09 21:46:59 +0200

    Initial commit

---
+++ src/main/kotlin/GameContainer.kt
@@ -0,0 +1,56 @@
+import java.lang.Runnable
+import java.lang.InterruptedException
+import kotlin.jvm.JvmStatic
+
+class GameContainer : Runnable {
+    private var thread: Thread? = null
+    private var running = false
+    private val UPDATE_CAP = 1.0 / 60.0
+    fun start() {
+        thread = Thread(this)
+        thread!!.run()
+    }
+
+    fun stop() {}
+    override fun run() {
+        running = true
+        var render = false
+        var firstTime = 0.0
+        var lastTime = System.nanoTime() / 1000000000.0
+        var passedTime = 0.0
+        var unprocessedTime = 0.0
+        while (running) {
+            firstTime = System.nanoTime() / 1000000000.0
+            passedTime = firstTime - lastTime
+            lastTime = firstTime
+            unprocessedTime += passedTime
+            while (unprocessedTime >= UPDATE_CAP) {
+                unprocessedTime += UPDATE_CAP
+                render = true
+                println("Update")
+
+
+                //TODO: Update Game
+            }
+            if (render) {
+                //TODO: Render Game
+            } else {
+                try {
+                    Thread.sleep(1)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        dispose()
+    }
+
+    private fun dispose() {}
+
+
+}
+
+public fun main(){
+    val gc = GameContainer()
+    gc.start()
+}
