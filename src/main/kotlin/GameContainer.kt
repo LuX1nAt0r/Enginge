166ed34ec1fbd32cebbe9c6ddf3c039fa9aaa884
LuX1nAt0r <l.landsteiner@t-online.de>
2021-06-10 20:17:24 +0200

    Added fps counter

--- src/main/kotlin/GameContainer.kt
+++ src/main/kotlin/GameContainer.kt
@@ -6,6 +6,8 @@ class GameContainer : Runnable {
     private var thread: Thread? = null
     private var running = false
     private val UPDATE_CAP = 1.0 / 60.0
+
+
     fun start() {
         thread = Thread(this)
         thread!!.run()
@@ -19,21 +21,44 @@ class GameContainer : Runnable {
         var lastTime = System.nanoTime() / 1000000000.0
         var passedTime = 0.0
         var unprocessedTime = 0.0
+
+        var frametime:Double = 0.0
+        var frames = 0
+        var fps = 0
+
+
         while (running) {
+
+            render = false
+
             firstTime = System.nanoTime() / 1000000000.0
             passedTime = firstTime - lastTime
             lastTime = firstTime
             unprocessedTime += passedTime
+            frametime += passedTime
+
+
             while (unprocessedTime >= UPDATE_CAP) {
-                unprocessedTime += UPDATE_CAP
+                unprocessedTime -= UPDATE_CAP
                 render = true
-                println("Update")
+
 
 
                 //TODO: Update Game
+                if (frametime >= 1.0){
+                    frametime = 0.0
+                    fps = frames
+                    frames = 0
+                    println("FPS: $fps")
+                }
+
             }
             if (render) {
                 //TODO: Render Game
+
+                frames++
+
+
             } else {
                 try {
                     Thread.sleep(1)
