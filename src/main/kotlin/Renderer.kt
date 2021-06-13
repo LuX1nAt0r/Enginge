562a33f1e70347e54f4e4141a16d8dcaa3a56a4e
LuX1nAt0r <l.landsteiner@t-online.de>
2021-06-13 18:36:56 +0200

    Currently adding the renderer

---
+++ src/main/kotlin/Renderer.kt
@@ -0,0 +1,18 @@
+
+
+class Renderer {
+
+    private var pW: Int? =null
+    private var pH: Int? =null
+    private var p: IntArray? =null
+
+    public fun Renderer(gc: GameContainer){
+
+        pW = gc.width
+        pH = gc.height
+
+    }
+
+
+
+}
