d042afd3d362dff1d0a91a8f4a8a0df47d3a4243
LuX1nAt0r <l.landsteiner@t-online.de>
2021-06-13 18:32:58 +0200

    Added Window

--- src/main/kotlin/GameContainer.kt
+++ src/main/kotlin/GameContainer.kt
@@ -1,30 +1,45 @@
+import java.awt.SystemColor.window
 import java.lang.Runnable
 import java.lang.InterruptedException
 import kotlin.jvm.JvmStatic
 
 class GameContainer : Runnable {
     private var thread: Thread? = null
+    private lateinit var window: Window
+
+
     private var running = false
     private val UPDATE_CAP = 1.0 / 60.0
 
+    var width = 320
+    var height = 240
+    var scale : Float = 3f
+    var title = "LukasEngine v1.0"
+
 
     fun start() {
+        window = Window()
+        window.Window(this)
+
         thread = Thread(this)
         thread!!.run()
     }
 
+
     fun stop() {}
+
+
     override fun run() {
         running = true
         var render = false
         var firstTime = 0.0
         var lastTime = System.nanoTime() / 1000000000.0
-        var passedTime = 0.0
+        var passedTime: Double
         var unprocessedTime = 0.0
 
         var frametime:Double = 0.0
         var frames = 0
-        var fps = 0
+        var fps: Int
 
 
         while (running) {
@@ -56,7 +71,8 @@ class GameContainer : Runnable {
             if (render) {
                 //TODO: Render Game
 
-                frames++
+                    window.update()
+                    frames++
 
 
             } else {
---
+++ src/main/kotlin/Window.kt
@@ -0,0 +1,50 @@
+import java.awt.BorderLayout
+import java.awt.Canvas
+import java.awt.Dimension
+import java.awt.Graphics
+import java.awt.image.BufferStrategy
+import java.awt.image.BufferedImage
+import javax.swing.JFrame
+
+class Window() {
+
+    private lateinit var frame :JFrame
+    private var image : BufferedImage? = null
+    private lateinit var canvas: Canvas
+    private var bs: BufferStrategy? =null
+    private var g: Graphics? =null
+
+
+
+    fun Window(gc: GameContainer){
+
+        image = BufferedImage(gc.width, gc.height, BufferedImage.TYPE_INT_RGB)
+        canvas = Canvas()
+        var s: Dimension = Dimension((gc.width * gc.scale).toInt(), (gc.height * gc.scale).toInt())
+        canvas.preferredSize = s
+        canvas.maximumSize = s
+        canvas.minimumSize = s
+
+        frame = JFrame(gc.title)
+        frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        frame.layout = BorderLayout()
+        frame.add(canvas, BorderLayout.CENTER)
+        frame.pack()
+        frame.setLocationRelativeTo(null)
+        frame.isResizable = false
+        frame.isVisible = true
+
+        canvas.createBufferStrategy(2)
+        bs = canvas.bufferStrategy
+        g = bs?.drawGraphics
+
+    }
+
+
+    fun update(){
+        g?.drawImage(image,0, 0, canvas.width,  canvas.height, null)
+        bs?.show()
+    }
+
+
+}
